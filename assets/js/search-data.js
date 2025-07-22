8ac4ec5cbc426c802c2293e30c0abf5d97e82aa0
eborbath <eborbath@users.noreply.github.com>
2025-07-22 19:19:34 +0000

    Deploying to gh-pages from @ eborbath/eborbath.github.io@a4e170b1a60d3472fa5a2992ad6bff9bbc43f9ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -222,13 +222,6 @@ ninja.data = [{
           window.open("https://bsky.app/profile/eborbath.bsky.social", "_blank");
         },
       },{
-        id: 'social-x',
-        title: 'X',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://twitter.com/eborbath", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
